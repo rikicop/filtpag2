f7ef887a0a22bfce34741d6ca731085ae8dce57c
rikicop <ruperto1@protonmail.com>
2021-04-05 11:40:11 -0400

    quite el reveal de contact

--- static/js/main.js
+++ static/js/main.js
@@ -72,11 +72,11 @@ sr.reveal('.skills__img', {delay: 400})
 /*SCROLL PORTFOLIO*/
  sr.reveal('.portfolio__img', {interval: 200}) 
 
-/*SCROLL CONTACT*/
+/*SCROLL CONTACT
 sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval: 200})
 sr.reveal('.contact__input', {delay: 400})
-sr.reveal('.contact__button', {delay: 600}) 
+sr.reveal('.contact__button', {delay: 600}) */
 
  
 
