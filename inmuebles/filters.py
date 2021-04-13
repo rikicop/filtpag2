5ba1ac958b63e972e8313cfbea6ca54aa2d811b0
rikicop <ruperto1@protonmail.com>
2021-04-13 19:34:43 -0400

    filters

--- inmuebles/filters.py
+++ inmuebles/filters.py
@@ -7,11 +7,18 @@ from .models import Inmueble
 OJEDA = 'Ciudad Ojeda'
 TAMARE ='Tamare'
 TIAJUANA ='Tía Juana'
-UBI_CHOICES = [(OJEDA,OJEDA),(TAMARE, TAMARE),(TIAJUANA, TIAJUANA)]
-
+LAGUNILLAS ='Lagunillas'
+MARACAIBO ='Maracaibo'
+CABIMAS = 'Cabimas'
+UBI_CHOICES = [(OJEDA,OJEDA),(TAMARE, TAMARE),(TIAJUANA, TIAJUANA),(LAGUNILLAS, LAGUNILLAS),(MARACAIBO,MARACAIBO),(CABIMAS,CABIMAS)]
 NUEVO = 'Nuevo'
 USADO = 'Usado'
 EDO_CHOICES = [ (NUEVO,NUEVO),(USADO, USADO),]
+
+VENTA = 'Venta'
+ALQUILER = 'Alquiler'
+PROPOSITO_CHOICES = [ (VENTA,VENTA),(ALQUILER, ALQUILER),]
+
 
 CASA = 'Casa'
 APARTAMENTO= 'Apartamento'
@@ -23,9 +30,10 @@ TIPO_CHOICES = [(CASA,CASA),(APARTAMENTO, APARTAMENTO), (LOTE, LOTE), (OFICINA,O
 class InmuebleFilter(filters.FilterSet):
     codigo = filters.CharFilter(label='No. de Código')
     ubicacion = filters.ChoiceFilter(choices=UBI_CHOICES, label='Zona,Ciudad Ojeda,Tamare...')
-    edo = filters.ChoiceFilter(choices=EDO_CHOICES, label='Nuevo/Usado/Alquiler...')
+    #edo = filters.ChoiceFilter(choices=EDO_CHOICES, label='Venta/Alquiler/...')
+    proposito = filters.ChoiceFilter(choices=PROPOSITO_CHOICES, label='Venta o Alquiler')
     tipo = filters.ChoiceFilter(choices=TIPO_CHOICES, label='Casa,Apartamento...')
     
     class Meta:
         model = Inmueble
-        fields =['tipo','ubicacion', 'edo','codigo']
+        fields =['tipo','ubicacion', 'proposito','codigo']
